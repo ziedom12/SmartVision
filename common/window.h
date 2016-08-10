cb05d3b243e00dadb746a8cf902a326c41a5228f
ziedom12 <zied0omrane@gmail.com>
2016-08-10 08:53:17 +0100

    Update window.h

--- common/window.h
+++ common/window.h
@@ -2,7 +2,8 @@
 #define _WINDOW_
 #include "opencv2/opencv.hpp"
 #include "observable.h"
-
+#include <vector>
+#include <tuple>
 using namespace cv ;
 
 namespace SmartVision {
@@ -15,10 +16,18 @@ namespace SmartVision {
 		void update(observable<Mat> &obs , const Mat& im){
 			imshow(name,im);
 		}
+		void update(observable<tuple<Mat,Mat,vector<Rect_<int> > > > &obs , const tuple<Mat,Mat,vector<Rect_<int> > >& in){
+			Mat show = get<0>(in);
+			for(auto face : get<2>(in)){
+				rectangle(show,face,CV_RGB(255,0,0),3) ;
+			}
+			
+			imshow(name,show);
+		}
+
 	} ;
 }
 
 
 #endif
-
 
